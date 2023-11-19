a45831a29ab57e8126612f71697246764946fbdc
Thomas Saunders <thomas@vesula.com>
2023-11-19 18:52:36 +0000

    Add basic menu

--- BPlan_Geography_Processor/Program.cs
+++ BPlan_Geography_Processor/Program.cs
@@ -6,7 +6,22 @@ namespace BPlan_Geography_Processor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] mainMenuItems = new string[] { "Check Location Definitions", "Build TLK Path", "Exit"};
+
+            Menu mainMenu = new Menu(mainMenuItems, "BPlan Geography Data Processor");
+
+            switch (mainMenu.displayMenu())
+            {
+                case 1:
+                    break;
+                case 2:
+                    break; 
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                default: 
+                    break;
+            }
         }
     }
 }
